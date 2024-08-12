c1c4f206e93848159f0d770d87d05981ac615f2d
Avedis <83277689+Avedis92@users.noreply.github.com>
2024-08-12 21:19:58 +0400

    create product route v1 (#1)

---
+++ src/services/productService.ts
@@ -0,0 +1,33 @@
+import { IProduct, CategoryType } from "../shared/types.ts";
+
+class ProductService {
+  getProductByIdService(id: string): IProduct {
+    return {
+      id,
+      title: "pants",
+      description: "skinny pants",
+      price: 12,
+      quantity: 4,
+      rating: 4.5,
+      category: "pants",
+      imageSource: "blabla",
+    };
+  }
+  getProductsByCategoryService(category: CategoryType): IProduct[] {
+    return [
+      {
+        id: "1",
+        title: "pants",
+        description: "skinny pants",
+        price: 12,
+        quantity: 4,
+        rating: 4.5,
+        category,
+        imageSource: "blabla",
+      },
+    ];
+  }
+}
+
+const productService = new ProductService();
+export default productService;
