2c7692be10a76d1b9240529c8961bd64be2347d6
Avedis <83277689+Avedis92@users.noreply.github.com>
2024-08-15 17:28:37 +0400

    Add cors (#5)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as Dotenv from "dotenv";
+import cors from "cors";
 import bodyParser from "body-parser";
 import productRouter from "./routes/productRoute.js";
 import serverless from "serverless-http";
@@ -9,7 +10,7 @@ Dotenv.config();
 const isLocalDev = process.env.IS_LOCAL === "true";
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/v1/products", productRouter);
