a983f01b42a75bf9c1dd0e08fe64f338df752035
Avedis <83277689+Avedis92@users.noreply.github.com>
2024-08-20 19:03:26 +0400

    Add get all products endpoint (#10)

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -8,6 +8,7 @@ productRouter.get(
   "/categories/:category",
   productController.getProductsByCategoryController()
 );
+productRouter.get("/", productController.getAllProducts());
 productRouter.post("/add", productController.addProductController());
 
 export default productRouter;
